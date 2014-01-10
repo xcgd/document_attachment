8f1048f6be4a55d3938817fdd1d5cf0dbf1e2d35
Anael Lorimier <anael.lorimier@xcg-consulting.fr>
2014-01-10 11:13:51 +0100

    Initial import

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Document Attachment',
+    'version': '0.1',
+    'author': 'XCG Consulting',
+    'category': 'Document',
+    'description': """ enchancements to the ir.attachment module
+    to provide admin docs
+    """,
+    'website': 'http://www.openerp-experts.com',
+    'depends': [
+        'base',
+        'ir_attachment',
+    ],
+    'data': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+}
