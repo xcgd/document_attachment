3699836b6977868b078491237739c09526b15360
Vincent Lhote-Hatakeyama <vincent.lhote@xcg-consulting.fr>
2014-09-26 10:44:04 +0200

    Production release (1.2)

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Document Attachment',
-    'version': '1.1.3',
+    'version': '1.2',
     'author': 'XCG Consulting',
     'category': 'Dependency',
     'description': """Enchancements to the ir.attachment module
