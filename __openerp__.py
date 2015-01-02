4946adddd889db89d65764f3a680ccc6853ea949
Vincent Hatakeyama <vincent.lhote@xcg-consulting.fr>
2015-01-02 15:41:21 +0100

    Update category and website on module description

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
     'name': 'Document Attachment',
     'version': '1.2',
     'author': 'XCG Consulting',
-    'category': 'Dependency',
+    'category': 'Hidden/Dependency',
     'description': """Enchancements to the ir.attachment module
 to manage kinds of attachments that can be linked with OpenERP objects.
 
@@ -15,7 +15,7 @@ Document attachments are displayed in a many2many field; it can optionally be
 changed to work like a one2many field by using the
 "domain="[('res_id', '=', id)]" attribute.
     """,
-    'website': 'www.odoo.consulting/',
+    'website': 'http://odoo.consulting/',
     'depends': [
         'base',
         'document',
